b1f5bc2045be51aaa8feebe046fdba17df9e68b7
kabirsumn <kabirinnow26@gmail.com>
2022-01-25 01:08:07 +0600

    some style changed in login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -21,6 +21,8 @@
         required
       />
     </label>
+
+    <button class="btn" type="submit">Log in</button>
   </form>
 </template>
 
@@ -30,6 +32,7 @@ export default {};
 
 <style>
 #form {
+  color: #666;
   width: 400px;
   display: flex;
   flex-direction: column;
@@ -37,21 +40,35 @@ export default {};
   justify-content: center;
   gap: 1rem;
   padding: 0 1rem 1.75rem;
-  border: 1px solid #555;
+  border: 1px solid #d1bfbf;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
 }
 label {
   width: 100%;
 }
 #form input {
+  color: #4e3b3b;
   width: 100%;
   height: 40px;
   padding: 0 30px 0 5px;
   border: 2px solid #c4c4c4;
   border-radius: 8px;
   outline: none;
+  font-size: 1.25em;
 }
 
-#form input::placeholder {
-  font: 1.25em sans-serif;
+.btn {
+  background-color: #45e989;
+  color: #444;
+  font-size: 1.4em;
+  margin-left: auto;
+  padding: 0.5rem 0.75rem;
+  border: none;
+  border-radius: 6px;
+  outline: none;
+  cursor: pointer;
 }
 </style>
