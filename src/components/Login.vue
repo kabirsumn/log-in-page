e3522ab7d5f5b79610266dff74942d8d5f5eb006
kabirsumn <kabirinnow26@gmail.com>
2022-01-24 22:55:04 +0600

    login component created

---
+++ src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <form action="post" id="form">
+    <h3>Get start</h3>
+    <label for="username">
+      <p>User Name</p>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        placeholder="username"
+        required
+      />
+    </label>
+    <label for="password">
+      <p>Password</p>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="password"
+        required
+      />
+    </label>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+}
+</style>
