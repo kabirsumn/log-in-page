43b399b7e1918802234e8910a3537d2baf6d3cde
kabirsumn <kabirinnow26@gmail.com>
2022-01-24 23:55:11 +0600

    some changes in style

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -30,10 +30,28 @@ export default {};
 
 <style>
 #form {
+  width: 400px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 1rem;
+  padding: 0 1rem 1.75rem;
+  border: 1px solid #555;
+}
+label {
+  width: 100%;
+}
+#form input {
+  width: 100%;
+  height: 40px;
+  padding: 0 30px 0 5px;
+  border: 2px solid #c4c4c4;
+  border-radius: 8px;
+  outline: none;
+}
+
+#form input::placeholder {
+  font: 1.25em sans-serif;
 }
 </style>
