ef09476d84fb62ea870e79093798334643d0d0dd
kabirsumn <kabirinnow26@gmail.com>
2022-01-24 22:55:55 +0600

    global style added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/Global.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
